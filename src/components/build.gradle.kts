32818faae5cb6f939dfea7a9a74a547946f4d7d8
zhaoyong <zhaoyong@shulie.io>
2022-11-19 19:41:09 +0800

    添加日志依赖

--- src/components/build.gradle.kts
+++ src/components/build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
     implementation("io.shulie.flpt:jmeter-redis-tool")
     implementation("io.shulie.flpt:jmeter-executor-tool")
     implementation("io.shulie.takin:takin-message-sdk-kafka")
-    implementation("org.slf4j:jcl-over-slf4j")
+//    implementation("org.slf4j:jcl-over-slf4j")
     implementation("org.slf4j:slf4j-api")
     implementation("cn.hutool:hutool-all:5.4.0")
 
