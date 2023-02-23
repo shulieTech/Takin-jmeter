fdcfeccc767449e52cff6d5df0533a06fa9bce49
zhaoyong <zhaoyong@shulie.io>
2023-02-23 16:50:23 +0800

    Metrics发送逻辑

--- src/components/src/main/java/org/apache/jmeter/visualizers/backend/influxdb/HttpJsonMetricsSender.java
+++ src/components/src/main/java/org/apache/jmeter/visualizers/backend/influxdb/HttpJsonMetricsSender.java
@@ -17,45 +17,17 @@
 
 package org.apache.jmeter.visualizers.backend.influxdb;
 
-import java.io.PrintWriter;
-import java.net.ConnectException;
-import java.net.SocketException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.*;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-
 import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.shulie.takin.sdk.kafka.HttpSender;
 import io.shulie.takin.sdk.kafka.MessageSendCallBack;
 import io.shulie.takin.sdk.kafka.MessageSendService;
 import io.shulie.takin.sdk.kafka.impl.KafkaSendServiceFactory;
-import io.shulie.takin.sdk.kafka.impl.KafkaSendServiceImpl;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringEscapeUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.concurrent.FutureCallback;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
-import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
-import org.apache.http.impl.nio.conn.PoolingNHttpClientConnectionManager;
-import org.apache.http.impl.nio.reactor.DefaultConnectingIOReactor;
-import org.apache.http.impl.nio.reactor.IOReactorConfig;
-import org.apache.http.nio.reactor.ConnectingIOReactor;
-import org.apache.http.util.EntityUtils;
-import org.apache.jmeter.report.utils.MetricUtils;
 import org.apache.jmeter.shulie.constants.PressureConstants;
 import org.apache.jmeter.shulie.util.HttpNotifyTroCloudUtils;
 import org.apache.jmeter.threads.JMeterContextService;
-import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jmeter.visualizers.backend.influxdb.entity.AbstractMetrics;
 import org.apache.jmeter.visualizers.backend.influxdb.entity.EventMetrics;
 import org.apache.jmeter.visualizers.backend.influxdb.entity.ResponseMetrics;
@@ -63,7 +35,10 @@ import org.apache.jmeter.visualizers.backend.influxdb.tro.JacksonUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 class HttpJsonMetricsSender extends AbstractInfluxdbMetricsSender {
     private static final Logger log = LoggerFactory.getLogger(HttpJsonMetricsSender.class);
@@ -85,15 +60,15 @@ class HttpJsonMetricsSender extends AbstractInfluxdbMetricsSender {
 
     private List<AbstractMetrics> metrics = new ArrayList<>();
 
-    private HttpPost httpRequest;
-    private CloseableHttpAsyncClient httpClient;
-    private URL url;
-    private String token;
-    private Integer SEND_INTERVAL;
-    private Long pressureId;
-    private Long pressureExampleId;
+//    private HttpPost httpRequest;
+//    private CloseableHttpAsyncClient httpClient;
+//    private URL url;
+//    private String token;
+//    private Integer SEND_INTERVAL;
+//    private Long pressureId;
+//    private Long pressureExampleId;
 
-    private Future<HttpResponse> lastRequest;
+//    private Future<HttpResponse> lastRequest;
     private HttpJsonMetricsSenderThread thread;
     private PrintWriter pw;
     private Long jobId;
@@ -142,27 +117,27 @@ class HttpJsonMetricsSender extends AbstractInfluxdbMetricsSender {
         }
     }
 
-    /**
-     * @param url   {@link URL} Influxdb Url
-     * @param token Influxdb 2.0 authorization token
-     * @return {@link HttpPost}
-     * @throws URISyntaxException
-     */
-    private HttpPost createRequest(URL url, String token) throws URISyntaxException {
-        RequestConfig defaultRequestConfig = RequestConfig.custom()
-                .setConnectTimeout(JMeterUtils.getPropDefault("backend_influxdb.connection_timeout", BACKEND_CONNECTION_TIMEOUT))
-                .setSocketTimeout(JMeterUtils.getPropDefault("backend_influxdb.socket_timeout", BACKEND_SOCKET_TIMEOUT))
-                .setConnectionRequestTimeout(JMeterUtils.getPropDefault("backend_influxdb.connection_request_timeout", 100))
-                .build();
-
-        HttpPost currentHttpRequest = new HttpPost(url.toURI());
-        currentHttpRequest.setConfig(defaultRequestConfig);
-        if (StringUtils.isNotBlank(token)) {
-            currentHttpRequest.setHeader(AUTHORIZATION_HEADER_NAME, AUTHORIZATION_HEADER_VALUE + token);
-        }
-        log.debug("Created Collection centre MetricsSender with url: {}", url);
-        return currentHttpRequest;
-    }
+//    /**
+//     * @param url   {@link URL} Influxdb Url
+//     * @param token Influxdb 2.0 authorization token
+//     * @return {@link HttpPost}
+//     * @throws URISyntaxException
+//     */
+//    private HttpPost createRequest(URL url, String token) throws URISyntaxException {
+//        RequestConfig defaultRequestConfig = RequestConfig.custom()
+//                .setConnectTimeout(JMeterUtils.getPropDefault("backend_influxdb.connection_timeout", BACKEND_CONNECTION_TIMEOUT))
+//                .setSocketTimeout(JMeterUtils.getPropDefault("backend_influxdb.socket_timeout", BACKEND_SOCKET_TIMEOUT))
+//                .setConnectionRequestTimeout(JMeterUtils.getPropDefault("backend_influxdb.connection_request_timeout", 100))
+//                .build();
+//
+//        HttpPost currentHttpRequest = new HttpPost(url.toURI());
+//        currentHttpRequest.setConfig(defaultRequestConfig);
+//        if (StringUtils.isNotBlank(token)) {
+//            currentHttpRequest.setHeader(AUTHORIZATION_HEADER_NAME, AUTHORIZATION_HEADER_VALUE + token);
+//        }
+//        log.debug("Created Collection centre MetricsSender with url: {}", url);
+//        return currentHttpRequest;
+//    }
 
     @Override
     public void addMetric(ResponseMetrics responseMetrics) {
@@ -197,7 +172,7 @@ class HttpJsonMetricsSender extends AbstractInfluxdbMetricsSender {
         //        writeAndSendMetrics(copyMetrics);
     }
 
-    //TODO mark by 李鹏 这里改为直接向influxdb写数据 而不是传到cloud
+//    //TODO mark by 李鹏 这里改为直接向influxdb写数据 而不是传到cloud
 //    public boolean writeAndSendMetrics(List<AbstractMetrics> copyMetrics, int times) {
 //        boolean flag = false;
 //        String sendData = "";
@@ -238,89 +213,87 @@ class HttpJsonMetricsSender extends AbstractInfluxdbMetricsSender {
             messageSendService.send("/notify/job/pressure/metrics/upload_old", new HashMap<>(), JSON.toJSONString(body), new MessageSendCallBack() {
                 @Override
                 public void success() {
-                    log.info("发送Metrics数据成功，metrics size为" + metrics.size());
                     flag[0] = true;
                 }
 
                 @Override
                 public void fail(String errorMessage) {
+                    log.error("发送Metrics数据出现异常,数据写入文件:{}", errorMessage);
                     flag[0] = false;
-                    log.error("发送Metrics数据出现异常:{}", errorMessage);
+                    if (times > 5 && Objects.nonNull(pw)) {
+                        pw.write(sendData + "\r\n");
+                        pw.flush();
+                        flag[0] = true;
+                    }
                 }
             }, new HttpSender() {
                 @Override
                 public void sendMessage() {}
             });
-            if (!flag[0] && times > 5 && Objects.nonNull(pw)) {
-                log.error("发送Metrics数据出现异常且超过失败次数,数据写入文件");
-                pw.write(sendData + "\r\n");
-                pw.flush();
-                return !flag[0];
-            }
             return flag[0];
         } catch (JsonProcessingException e) {
             log.error("发送Metrics数据出现异常",e);
-        }
-        return false;
-    }
-
-    private class MetricsSenderCallback implements FutureCallback<HttpResponse> {
-        private HttpPost httpRequest;
-        private List<AbstractMetrics> copyMetrics;
-
-        public MetricsSenderCallback(HttpPost httpRequest, List<AbstractMetrics> copyMetrics) {
-            this.httpRequest = httpRequest;
-            this.copyMetrics = copyMetrics;
-        }
-
-        @Override
-        public void completed(final HttpResponse response) {
-            int code = response.getStatusLine().getStatusCode();
-            /*
-             * If your write request received HTTP
-             * 204 No Content: it was a success!
-             * 4xx: InfluxDB could not understand the request.
-             * 5xx: The system is overloaded or significantly impaired.
-             */
-            if (MetricUtils.isSuccessCode(code)) {
-                //查看日志
-                log.info("Success, number of metrics written: {}", copyMetrics.size());
-                //                        if (log.isDebugEnabled()) {
-                //                            log.debug("Success, number of metrics written: {}", copyMetrics.size());
-                //                        }
-            } else {
-                log.error(
-                        "Error writing metrics to Collection centre Url: {}, responseCode: {}, responseBody: {}",
-                        url, code, getBody(response));
-            }
-        }
-
-        @Override
-        public void failed(final Exception ex) {
-            log.error("failed to send data to Collection centre server.", ex);
-        }
-
-        @Override
-        public void cancelled() {
-            log.warn("Request to Collection centre server was cancelled");
+            return true;
         }
     }
 
-    /**
-     * @param response HttpResponse
-     * @return String entity Body if any
-     */
-    private static String getBody(final HttpResponse response) {
-        String body = "";
-        try {
-            if (response != null && response.getEntity() != null) {
-                body = EntityUtils.toString(response.getEntity());
-            }
-        } catch (Exception e) { // NOSONAR
-            // NOOP
-        }
-        return body;
-    }
+//    private class MetricsSenderCallback implements FutureCallback<HttpResponse> {
+//        private HttpPost httpRequest;
+//        private List<AbstractMetrics> copyMetrics;
+//
+//        public MetricsSenderCallback(HttpPost httpRequest, List<AbstractMetrics> copyMetrics) {
+//            this.httpRequest = httpRequest;
+//            this.copyMetrics = copyMetrics;
+//        }
+//
+//        @Override
+//        public void completed(final HttpResponse response) {
+//            int code = response.getStatusLine().getStatusCode();
+//            /*
+//             * If your write request received HTTP
+//             * 204 No Content: it was a success!
+//             * 4xx: InfluxDB could not understand the request.
+//             * 5xx: The system is overloaded or significantly impaired.
+//             */
+//            if (MetricUtils.isSuccessCode(code)) {
+//                //查看日志
+//                log.info("Success, number of metrics written: {}", copyMetrics.size());
+//                //                        if (log.isDebugEnabled()) {
+//                //                            log.debug("Success, number of metrics written: {}", copyMetrics.size());
+//                //                        }
+//            } else {
+//                log.error(
+//                        "Error writing metrics to Collection centre Url: {}, responseCode: {}, responseBody: {}",
+//                        url, code, getBody(response));
+//            }
+//        }
+//
+//        @Override
+//        public void failed(final Exception ex) {
+//            log.error("failed to send data to Collection centre server.", ex);
+//        }
+//
+//        @Override
+//        public void cancelled() {
+//            log.warn("Request to Collection centre server was cancelled");
+//        }
+//    }
+
+//    /**
+//     * @param response HttpResponse
+//     * @return String entity Body if any
+//     */
+//    private static String getBody(final HttpResponse response) {
+//        String body = "";
+//        try {
+//            if (response != null && response.getEntity() != null) {
+//                body = EntityUtils.toString(response.getEntity());
+//            }
+//        } catch (Exception e) { // NOSONAR
+//            // NOOP
+//        }
+//        return body;
+//    }
 
     @Override
     public void destroy() {
@@ -328,15 +301,15 @@ class HttpJsonMetricsSender extends AbstractInfluxdbMetricsSender {
         log.info("Destroying ");
         thread.destroy();
         messageSendService.stop();
-        try {
-            lastRequest.get(5, TimeUnit.SECONDS);
-        } catch (InterruptedException | ExecutionException | TimeoutException e) {
-            log.error("Error waiting for last request to be send to InfluxDB", e);
-        }
-        if (httpRequest != null) {
-            httpRequest.abort();
-        }
-        IOUtils.closeQuietly(httpClient);
+//        try {
+//            lastRequest.get(5, TimeUnit.SECONDS);
+//        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+//            log.error("Error waiting for last request to be send to InfluxDB", e);
+//        }
+//        if (httpRequest != null) {
+//            httpRequest.abort();
+//        }
+//        IOUtils.closeQuietly(httpClient);
     }
 
 }
