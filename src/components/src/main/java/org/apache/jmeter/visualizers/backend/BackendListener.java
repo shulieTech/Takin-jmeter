ee1cda846661f0c79ffa37bcd0814b9b974fb7f5
zxzghr <tian_ya_1990@126.com>
2025-12-11 19:14:34 +0800

    优化size

--- src/components/src/main/java/org/apache/jmeter/visualizers/backend/BackendListener.java
+++ src/components/src/main/java/org/apache/jmeter/visualizers/backend/BackendListener.java
@@ -99,7 +99,7 @@ public class BackendListener
      */
     private Class<?> clientClass;
 
-    public static final String DEFAULT_QUEUE_SIZE = "10000";
+    public static final String DEFAULT_QUEUE_SIZE = "20000";
 
     /**
      * Create unique object as marker for end of queue
